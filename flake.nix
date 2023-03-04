ffeb7865b53888b05b937c5aa06a9b82bfbbc3a3
Chris Dunphy <chris@megaparsec.ca>
2023-03-04 07:24:34 -0700

    Turn this project into a flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "This is a flake for building my pinyin-tone-marks Rust app";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell
+          {
+            # The packages we need for this project
+            buildInputs = with pkgs;
+              [
+                rustc
+                cargo
+                clippy
+                libiconv
+                rust-analyzer
+                rustfmt
+              ];
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          };
+      });
+}
