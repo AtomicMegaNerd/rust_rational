d23707434149de8ae3d999c8a075c51891e7c8ca
Chris Dunphy <chris@megaparsec.ca>
2023-04-11 07:28:29 -0600

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "This is a flake for building my pinyin-tone-marks Rust app";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
