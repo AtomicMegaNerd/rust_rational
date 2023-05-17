7da18409b05100400fc26d6a92c69b2128fc443a
Chris Dunphy <chris@megaparsec.ca>
2023-05-17 13:09:35 -0600

    Update flake

--- flake.nix
+++ flake.nix
@@ -17,7 +17,6 @@
               [
                 rustup
                 libiconv
-                rust-analyzer
               ];
             RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
           };
